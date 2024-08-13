cc36322f4bfd43865b5c397edb6d2ba29392e63e
Petro-Herman <germanpetr24@gmail.com>
2024-08-13 22:39:09 +0300

    д

---
+++ src/components/Alert.jsx
@@ -0,0 +1,17 @@
+const alertStyles = {
+  margin: 8,
+  padding: "12px 16px",
+  borderRadius: 4,
+  backgroundColor: "gray",
+  color: "white",
+};
+
+export const Alert = ({ children }) => {
+  return <p style={alertStyles}>{children}</p>;
+};
+
+const getBgColor = variant => {
+    switch (variant) {
+        case
+    }
+}
---
+++ src/components/Product.jsx
@@ -0,0 +1,17 @@
+export default function Product({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) {
+  // const price = 999;
+  // const imgUrl =
+  //   "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
+    </>
+  );
+}
