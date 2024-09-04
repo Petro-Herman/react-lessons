d2adef9ed56a2a9fa967c02c9b34ffab0e505f5e
Petro-Herman <159651446+Petro-Herman@users.noreply.github.com>
2024-09-04 23:51:40 +0300

    useRef

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,31 @@
 import { useEffect, useMemo, useRef, useState } from "react";
+import Player from "../Player/Player";
 
 export default function App() {
   //   const [planets, setPlanets] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
   //   const [query, setQuery] = useState("");
   //     const [clicks, setClicks] = useState(0);
-  const [value, setValue] = useState(0);
-  const btnRef = useRef();
-
-  console.log("App: ", btnRef.current);
+  // const [value, setValue] = useState(0);
+  // const btnRef = useRef();
+  const valueRef = useRef(0);
 
   useEffect(() => {
-    console.log("useEffect: ", btnRef.current);
+    console.log(valueRef.current);
   });
 
   const handleClick = () => {
-    console.log("handleclick: ", btnRef.current);
+    valueRef.current += 1;
   };
+
+  // console.log("App: ", btnRef.current);
+
+  // useEffect(() => {
+  //   console.log("useEffect: ", btnRef.current);
+  // });
+
+  // const handleClick = () => {
+  //   console.log("handleclick: ", btnRef.current);
+  // };
 
   // const filteredPlanets = useMemo(() =>
   //     planets.filter((planet) => planet.includes(query)),
@@ -24,12 +34,14 @@ export default function App() {
 
   return (
     <>
-      <button onClick={() => setValue(value + 1)}>
+    <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
+<button onClick={handleClick}>Click to update ref value</button>
+      {/* <button onClick={() => setValue(value + 1)}>
         Update value to trigger re-render
       </button>
       <button ref={btnRef} onClick={handleClick}>
         Button with ref
-      </button>
+      </button> */}
       {/* <button onClick={() => setClicks(clicks + 1)}>
         Number of clicks: {clicks}
       </button>
